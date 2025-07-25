4f08b5a857999fdd6186a83076763e32516dca79
threatthreat <grezy8007@gmail.com>
2025-07-25 10:14:30 -0700

    Update info.py

--- info.py
+++ info.py
@@ -86,7 +86,7 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/Bombardiro_Crocodiloa
 # ============================
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortxlinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'd73e70a35dc3877fa14afbf51fa8ec312c94780c')
+SHORTLINK_API = environ.get('SHORTLINK_API', '0123cbbcb7da6b4d5eb066e3f5545ef53ecee5bd')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/dreamcinezone')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 
